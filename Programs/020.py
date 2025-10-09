6dd838cafcb4a8a870b0be069fcc28605194ed15
Prashant Gohel <smaterial123321@gmail.com>
2025-10-09 21:38:24 +0530

    020.py

---
+++ Programs/020.py
@@ -0,0 +1,20 @@
+"""
+Write a program which takes input of income and gender, and display message whether income is taxable or not as per following condition:
+
+if gender = male and income is greater than 700000 and if gender = female and income is greater than 500000
+display message taxable.
+"""
+
+income = int(input("Enter your income: "))
+gender = input("Enter your gender. Type 'M' for Male and Type 'F' for female: ").lower()
+
+if gender == 'm':
+    if income >= 700000:
+        print("Txable income.")
+    else:
+        print("Non-taxable income.")
+else:
+    if income >= 500000:
+        print("Txable income.")
+    else:
+        print("Non-taxable income.")
