bfea2cde8b1617803be9ba0bc9b2c4d4be158b72
Prashant Gohel <smaterial123321@gmail.com>
2025-10-09 21:24:07 +0530

    018

---
+++ Programs/018.py
@@ -0,0 +1,10 @@
+"""
+Write a program which takes input of 1 number and display whether the number is positive or negative.    
+"""
+
+num = int(input("Enter any number: "))
+
+if num < 0:
+    print(f"{num} is a Negative number.")
+else:
+    print(f"{num} is a Positive number.")
