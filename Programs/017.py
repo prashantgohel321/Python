965911132bb93795368273c8a1744d5dad0cb4e7
Prashant Gohel <smaterial123321@gmail.com>
2025-10-09 21:22:08 +0530

    Program 17

---
+++ Programs/017.py
@@ -0,0 +1,6 @@
+"""
+Write a program which takes input value for age from user and display message if user is eligible for vote.    
+"""
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible for vote.")
